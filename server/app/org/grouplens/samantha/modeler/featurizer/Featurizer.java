d6e32db4edd87188954d1fdfcfb91df20d43e907
Qian Zhao <zhaox331@umn.edu>
2017-02-06 17:44:36 -0600

    added a todo on featurizer

--- server/app/org/grouplens/samantha/modeler/featurizer/Featurizer.java
+++ server/app/org/grouplens/samantha/modeler/featurizer/Featurizer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.grouplens.samantha.modeler.common.LearningInstance;
 
 //TODO: warn if feature is not present
+//TODO: warn or error if label is not present when update is true
 public interface Featurizer {
     LearningInstance featurize(JsonNode entity, boolean update);
 }
