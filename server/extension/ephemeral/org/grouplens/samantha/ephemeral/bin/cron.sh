e8acf329a78ca12024301652974bc44ccb2ba6fd
Taavi Taijala <taavi@taijala.com>
2017-03-30 07:38:24 -0500

    Add argument validation and user-friendly error messages to cron.sh

--- server/extension/ephemeral/org/grouplens/samantha/ephemeral/bin/cron.sh
+++ server/extension/ephemeral/org/grouplens/samantha/ephemeral/bin/cron.sh
@@ -1,10 +1,24 @@
+#!/bin/sh
+if [ "$#" -lt 4 ]; then
+  echo "Usage: $0 datadir dbhost dbname samantha_host(s)..." >&2
+  exit 1
+fi
+if ! [ -e "$1" ]; then
+  echo "$1 not found" >&2
+  exit 1
+fi
+if ! [ -d "$1" ]; then
+  echo "$1 not a directory" >&2
+  exit 1
+fi
+
 # Get path of directory containing this script
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 
-PATH=$1; shift
+DATAPATH=$1; shift
 DBHOST=$1; shift
 DBNAME=$1; shift
 
-echo $DIR/dumpdb.py $PATH $DBHOST $DBNAME > $DIR/dumpdb.log
-echo $DIR/rebuild.py $PATH "$@" > $DIR/rebuild.log
+echo $DIR/dumpdb.py $DATAPATH $DBHOST $DBNAME > $DIR/dumpdb.log
+echo $DIR/rebuild.py $DATAPATH "$@" > $DIR/rebuild.log
