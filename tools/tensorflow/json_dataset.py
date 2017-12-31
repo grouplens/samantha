41847adce7f045b06c831ace1ba92a3c92e1b062
qian2015 <zhaox331@umn.edu>
2017-12-30 18:35:05 -0600

    json dataset and tensorflow dependency

---
+++ tools/tensorflow/json_dataset.py
@@ -0,0 +1,24 @@
+
+import json
+
+import tensorflow as tf
+
+from dataset import DataSet
+
+class JsonDataSet(DataSet):
+
+    def __init__(self, files=[]):
+        self.files = files
+
+    def next_batch(self):
+        for afile in self.files:
+            with open(afile) as fin:
+                for line in fin:
+                    obj = json.loads(line.strip())
+                    feed_dict = {}
+                    for key, val in obj.iteritems():
+                        feed_dict[key] = tf.Constant(val)
+                    yield feed_dict
+
+    def reset(self):
+        pass
