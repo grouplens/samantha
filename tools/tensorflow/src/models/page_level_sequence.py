01ef71225915c71b23601836a8f4471c281f79f7
qian2015 <zhaox331@umn.edu>
2018-01-06 08:50:34 -0600

    added scoping in the page level sequence model

--- tools/tensorflow/src/models/page_level_sequence.py
+++ tools/tensorflow/src/models/page_level_sequence.py
@@ -42,7 +42,7 @@ class PageLevelSequenceModelBuilder(ModelBuilder):
         rnn_layer = tf.keras.layers.GRU(self._rnn_size, return_sequences=True)
         return rnn_layer(input)
 
-    def _compute_map_metrics(self, event, labels, logits, metric):
+    def _compute_map_metrics(self, labels, logits, metric):
         K = metric.split('@')[1].split(',')
         updates = []
         expand_labels = tf.expand_dims(labels, 1)
@@ -59,7 +59,7 @@ class PageLevelSequenceModelBuilder(ModelBuilder):
             tf.summary.scalar('MAP@%s' % k, map_value)
         return updates
 
-    def _compute_event_loss(self, event, rnn_output, indices, labels, softmax, metrics=None):
+    def _compute_event_loss(self, rnn_output, indices, labels, softmax, metrics=None):
         valid_labels = tf.gather_nd(labels, indices)
         batch_idx = tf.reshape(tf.slice(indices,
             begin=[0, 0],
@@ -76,7 +76,7 @@ class PageLevelSequenceModelBuilder(ModelBuilder):
         if metrics != None:
             for metric in metrics.split(' '):
                 if 'MAP' in metric:
-                    metric_update += self._compute_map_metrics(event, valid_labels, logits, metric)
+                    metric_update += self._compute_map_metrics(valid_labels, logits, metric)
         return loss, metric_update
 
     def _get_softmax_loss(self, sequence_length, rnn_output, label_by_event, softmax_by_event):
@@ -101,21 +101,19 @@ class PageLevelSequenceModelBuilder(ModelBuilder):
                 non_zeros_indices, tf.logical_and(step_idx > 0, step_idx <= step_split_limit))
             eval_indices = tf.boolean_mask(
                 non_zeros_indices, tf.logical_and(step_idx > step_split_limit, step_idx < step_length_limit))
-            with tf.name_scope(key):
+            with tf.variable_scope(key):
                 train_event_loss, _ = self._compute_event_loss(
-                    key, rnn_output, train_indices, label_by_event[key], softmax_by_event[key])
+                    rnn_output, train_indices, label_by_event[key], softmax_by_event[key])
                 eval_event_loss, metric_update = self._compute_event_loss(
-                    key, rnn_output, eval_indices, label_by_event[key],
+                    rnn_output, eval_indices, label_by_event[key],
                     softmax_by_event[key], metrics=self._eval_metrics)
             train_loss += train_event_loss
             eval_loss += eval_event_loss
             updates += metric_update
         train_loss /= len(self._item_events)
         eval_loss /= len(self._item_events)
-        with tf.name_scope('loss'):
-            tf.summary.scalar('train_loss', train_loss)
-            tf.summary.scalar('eval_loss', eval_loss)
-        self._test_tensors['eval_loss'] = eval_loss
+        tf.summary.scalar('train_loss', train_loss)
+        tf.summary.scalar('eval_loss', eval_loss)
         return train_loss, updates
 
     def _get_softmax_prediction(self, sequence_length, rnn_output, softmax_by_event):
@@ -130,50 +128,56 @@ class PageLevelSequenceModelBuilder(ModelBuilder):
         return probs_by_event[self._predicted_event]
 
     def build_model(self):
-        #get [batch, sequence_length, concatenated_embedding]
+        #inputs from feed_dict
         item_idx = {}
         for key in self._item_events:
             item_idx[key] = self._event_item(key)
-        #create item input embeddings
-        event_embedder = {}
-        for key in self._item_events:
-            event_embedder[key] = tf.keras.layers.Embedding(self._item_vocab_size, self._rnn_size)
-        #get embeddings for each event
-        event_embedding = []
-        for key in self._item_events:
-            event_embedding.append(event_embedder[key](item_idx[key]))
-        concated_event_embedding = tf.concat(event_embedding, 3)
-        item_input = tf.reshape(concated_event_embedding,
-                                [tf.shape(concated_event_embedding)[0],
-                                 tf.shape(concated_event_embedding)[1],
-                                 len(self._item_events) * self._page_size * self._rnn_size])
         user_idx = tf.placeholder(tf.int32, shape=(None, 1), name='user_idx')
-        #create user input embedding
-        user_embedder = tf.keras.layers.Embedding(self._user_vocab_size, self._rnn_size)
-        expanded_user = tf.tile(user_idx, [1, tf.shape(item_input)[1]])
-        expanded_user_embedding = user_embedder(expanded_user)
-        all_input = tf.concat([item_input, expanded_user_embedding], 2)
-
-        #go through relu to have [batch, sequence_length, relu_output]
-        relu_output = self._step_wise_relu(all_input)
-
-        #go through rnn and get all the states output [batch, sequence_length, rnn_state]
-        rnn_output = self._get_rnn_output(relu_output)
-
-        #create item softmax weights and biases
-        softmax_layer = {}
-        for key in self._item_events:
-            softmax_layer[key] = tf.keras.layers.Dense(self._item_vocab_size)
-
         #get sequence length as input for each sequence in the batch
         sequence_length = tf.placeholder(tf.int32, shape=(None, 1), name='sequence_length_val')
 
-        #get softmax losses for all steps excluding the zeros
-        loss, updates = self._get_softmax_loss(
-            sequence_length, rnn_output, item_idx, softmax_layer)
+        with tf.variable_scope('embedding'):
+            #create item input embeddings
+            event_embedder = {}
+            for key in self._item_events:
+                event_embedder[key] = tf.keras.layers.Embedding(self._item_vocab_size, self._rnn_size)
+            #get embeddings for each event
+            event_embedding = []
+            for key in self._item_events:
+                event_embedding.append(event_embedder[key](item_idx[key]))
+            concated_event_embedding = tf.concat(event_embedding, 3)
+            item_input = tf.reshape(concated_event_embedding,
+                                    [tf.shape(concated_event_embedding)[0],
+                                     tf.shape(concated_event_embedding)[1],
+                                     len(self._item_events) * self._page_size * self._rnn_size])
+            #create user input embedding
+            user_embedder = tf.keras.layers.Embedding(self._user_vocab_size, self._rnn_size)
+            expanded_user = tf.tile(user_idx, [1, tf.shape(item_input)[1]])
+            expanded_user_embedding = user_embedder(expanded_user)
+            all_input = tf.concat([item_input, expanded_user_embedding], 2)
 
-        #get softmax prediction by taking into account all steps in the sequences excluding the zeros
-        prediction = self._get_softmax_prediction(
-            sequence_length, rnn_output, softmax_layer)
+        #go through relu to have [batch, sequence_length, relu_output]
+        with tf.variable_scope('relu'):
+            relu_output = self._step_wise_relu(all_input)
+
+        #go through rnn and get all the states output [batch, sequence_length, rnn_state]
+        with tf.variable_scope('rnn'):
+            rnn_output = self._get_rnn_output(relu_output)
+
+        #create item softmax weights and biases
+        with tf.variable_scope('softmax'):
+            softmax_layer = {}
+            for key in self._item_events:
+                softmax_layer[key] = tf.keras.layers.Dense(self._item_vocab_size)
+
+            #get softmax losses for all steps excluding the zeros
+            with tf.variable_scope('loss'):
+                loss, updates = self._get_softmax_loss(
+                    sequence_length, rnn_output, item_idx, softmax_layer)
+
+            #get softmax prediction by taking into account all steps in the sequences excluding the zeros
+            with tf.variable_scope('prediction'):
+                prediction = self._get_softmax_prediction(
+                    sequence_length, rnn_output, softmax_layer)
 
         return loss, updates
--- tools/tensorflow/src/trainer.py
+++ tools/tensorflow/src/trainer.py
@@ -11,18 +11,14 @@ class ModelTrainer(object):
 
     def __init__(self,
                  train_data,
-                 eval_data=None,
                  tensorboard_dir='/tmp/tflearn_logs/',
                  builder=ModelBuilder(),
                  max_steps=1e7,
-                 learning_rate=0.01,
-                 steps_per_eval=1000):
+                 learning_rate=0.01):
         self.train_data = train_data
-        self.eval_data = eval_data
         self.tensorboard_dir = tensorboard_dir
         self.builder = builder
         self.max_steps = max_steps
-        self.steps_per_eval = steps_per_eval
         self.learning_rate = learning_rate
 
     def train(self, run_name=None):
@@ -44,8 +40,7 @@ class ModelTrainer(object):
                 if run_name is None:
                     run_name = ''.join(
                         random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-                train_writer = tf.summary.FileWriter(self.tensorboard_dir + run_name + '_train', graph)
-                eval_writer = tf.summary.FileWriter(self.tensorboard_dir + run_name + '_eval', graph)
+                train_writer = tf.summary.FileWriter(self.tensorboard_dir + run_name, graph)
                 logging.info('Initializing the model graph.')
                 print 'Initializing the model graph.'
                 session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
@@ -54,14 +49,6 @@ class ModelTrainer(object):
                 step = 0
                 while step < self.max_steps:
                     for train_batch in self.train_data.next_batch():
-                        if step % self.steps_per_eval == 1:
-                            logging.info('Evaluating on the evaluation data set.')
-                            print 'Evaluating on the evaluation data set.'
-                            if self.eval_data is not None:
-                                for eval_batch in self.eval_data.next_batch():
-                                    eval_summary = session.run(merged_summary, feed_dict=eval_batch)
-                                    eval_writer.add_summary(eval_summary, step)
-                                self.eval_data.reset()
                         train_vals = session.run(run_tensors, feed_dict=train_batch)
                         train_writer.add_summary(train_vals['merged_summary'], step)
                         step += 1
@@ -71,5 +58,4 @@ class ModelTrainer(object):
                             break
                     self.train_data.reset()
                 train_writer.close()
-                eval_writer.close()
         session.close()
