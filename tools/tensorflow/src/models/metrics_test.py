b7b75fc1ad0220a6272a5b2f388fd00954d412c3
qian2015 <zhaox331@umn.edu>
2018-02-19 16:46:46 -0600

    metrics test

--- tools/tensorflow/src/models/metrics_test.py
+++ tools/tensorflow/src/models/metrics_test.py
@@ -14,11 +14,31 @@ class MetricsTest(unittest.TestCase):
         with graph.as_default():
             session = tf.Session(graph=graph)
             with session.as_default():
-                labels = tf.random_uniform([10], dtype=tf.int64, maxval=99)
-                preds = tf.random_uniform([10, 100], dtype=tf.float32)
-                values, updates = compute_map_metrics(labels, preds, 'MAP@1,5,10')
-                session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
-                session.run([updates, values])
+                with tf.variable_scope('test1'):
+                    labels = tf.constant([2], dtype=tf.int64)
+                    preds = tf.constant([[0.01, 0.4, 0.1]])
+                    values, updates = compute_map_metrics(labels, preds, 'MAP@1,2')
+                    session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+                    session.run(updates)
+                    maps = session.run(values)
+                    self.assertEqual(maps[0], 0.0)
+                    self.assertEqual(maps[1], 0.5)
+                with tf.variable_scope('test2'):
+                    labels = tf.SparseTensor(
+                        tf.constant([[0, 2], [0, 5], [1, 0], [1, 2], [1, 4]], dtype=tf.int64),
+                        tf.constant([2, 3, 0, 7, 1], dtype=tf.int64),
+                        tf.constant([2, 6], dtype=tf.int64))
+                    preds = tf.constant([
+                        [0.3, 0.1, 0.9, 0.10, 0.2, 0.01, 0.003, 0.14],
+                        [0.01, 0.9, 0.1, 0.2, 0.38, 0.2, 0.48, 0.04]])
+                    values, updates = compute_map_metrics(labels, preds, 'MAP@1,7,8')
+                    session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+                    session.run(updates)
+                    maps = session.run(values)
+                    print maps
+                    self.assertEqual(maps[0], 0.416)
+                    self.assertEqual(maps[1], 0.547)
+                    self.assertEqual(maps[2], 0.610)
 
     def test_compute_ap_metrics(self):
         pass
