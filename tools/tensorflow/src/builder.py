eb50f5377276085e2ace4b7b3d2785542bec0522
Qian Zhao <zhaox331@umn.edu>
2018-03-07 16:23:02 -0600

    add training and init ops for dumping graph

--- tools/tensorflow/src/builder.py
+++ tools/tensorflow/src/builder.py
@@ -21,12 +21,16 @@ class ModelBuilder(object):
         return {}
 
     def build_optimizer(self, loss, learning_rate):
-        return tf.train.AdagradOptimizer(learning_rate).minimize(loss, name='upate_op')
+        return tf.train.AdagradOptimizer(learning_rate).minimize(loss, name='update_op')
 
     def dump_graph(self, file_path, learning_rate):
         graph = tf.Graph()
         with graph.as_default():
             loss, _ = self.build_model()
             self.build_optimizer(loss, learning_rate)
+            tf.group(
+                tf.global_variables_initializer(), tf.local_variables_initializer(),
+                name='init_op'
+            )
             with open(file_path, 'w') as fout:
                 fout.write(graph.as_graph_def().SerializeToString())
