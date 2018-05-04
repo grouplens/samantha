275856eab72b1b64350cf017e1e1fa7f29968a3b
qian2015 <zhaox331@umn.edu>
2018-05-03 19:30:38 -0500

    simulated data set

--- tools/tensorflow/src/datasets/svd_softmax_simulated.py
+++ tools/tensorflow/src/datasets/svd_softmax_simulated.py
@@ -1,12 +1,10 @@
 
 import random
 import logging
-import os
 import json
 import numpy as np
 
 from src.dataset import DataSet
-from src.datasets.json_files import JsonFilesDataSet
 
 logger = logging.getLogger('simulated')
 
@@ -17,7 +15,7 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
         if config is None:
             self._config = {
                 'embedding_dim': 5,
-                'user_vocab': 5,
+                'user_vocab': 8,
                 'item_vocab': 100,
                 'attr_vocab': 10,
                 'max_seq_len': 10,
@@ -31,17 +29,12 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
             self._config = config
         self._user_idx = 0
         self._item2attr = None
-        self._embeddings = {}
-        if os.path.exists(self._config['json_file']):
-            self._json_file_dataset = JsonFilesDataSet(files=[self._config['json_file']])
-        else:
-            self._json_file_dataset = None
-            self._embeddings = {
-                'user': np.random.rand(self._config['user_vocab'], self._config['embedding_dim']),
-                'item': np.random.rand(self._config['item_vocab'], self._config['embedding_dim']),
-                'attr': np.random.rand(self._config['attr_vocab'], self._config['embedding_dim']),
-            }
-            self._item2attr, self._attr2items = self._generate_item_attr()
+        self._embeddings = {
+            'user': np.random.rand(self._config['user_vocab'], self._config['embedding_dim']),
+            'item': np.random.rand(self._config['item_vocab'], self._config['embedding_dim']),
+            'attr': np.random.rand(self._config['attr_vocab'], self._config['embedding_dim']),
+        }
+        self._item2attr, self._attr2items = self._generate_item_attr()
 
     def _generate_item_attr(self):
         item2attr = [random.randint(0, self._config['attr_vocab'] - 1) for _ in range(
@@ -54,9 +47,9 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
         exp_logits = np.exp(logits)
         probs = np.divide(exp_logits, np.sum(exp_logits))
         dices = np.random.choice(range(len(probs)), size=[size], p=probs)
-        return dices
+        return list(dices)
 
-    def _generate_from_partial_softmax(self, input, weights, includes):
+    def _generate_from_sub_softmax(self, input, weights, includes):
         part_weights = []
         for idx in includes:
             part_weights.append(weights[idx])
@@ -66,7 +59,7 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
         dices = np.random.choice(range(len(probs)), size=[1], p=probs)
         return dices[0]
 
-    def _generate_item_attr(self, user_idx, size):
+    def _generate_user_item_attr(self, user_idx, size):
         if self._config['hierarchical']:
             attr_indices = self._generate_from_full_softmax(
                 self._embeddings['user'][user_idx],
@@ -86,27 +79,19 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
                 attr_indices.append(self._item2attr[item_idx])
         return item_indices, attr_indices
 
-    def next_batch(self):
-        if self._json_file_dataset is None:
-            with open(self._config['json_file'], 'w') as fout:
-                num_batches = self._config['user_vocab'] / self._config['batch_size']
-                for i in range(num_batches):
-                    seq_len = random.randint(self._config['min_seq_len'], self._config['max_seq_len'])
-                    seq_len *= self._config['page_size']
-                    batch = {'user_idx': [], 'item_idx': [], 'sequence_length_val': [], 'attr_idx': []}
-                    for l in range(self._config['batch_size']):
-                        self._user_idx += 1
-                        batch['user_idx'].append([self._user_idx])
-                        batch['sequence_length_val'].append([seq_len])
-                        item_indices, attr_indices = self._generate_item_attr(self._user_idx, seq_len)
-                        batch['item_idx'].append(item_indices)
-                        batch['attr_idx'].append(attr_indices)
-                    json.dump(batch, fout)
-                    yield batch
-        else:
-            yield self._json_file_dataset.next_batch()
-
-    def reset(self):
-        if os.path.exists(self._config['json_file']):
-            self._json_file_dataset = JsonFilesDataSet(files=[self._config['json_file']])
-        self._user_idx = 0
+    def generate(self):
+        with open(self._config['json_file'], 'w') as fout:
+            num_batches = self._config['user_vocab'] / self._config['batch_size']
+            for i in range(num_batches):
+                seq_len = random.randint(self._config['min_seq_len'], self._config['max_seq_len'])
+                seq_len *= self._config['page_size']
+                batch = {'user_idx': [], 'item_idx': [], 'sequence_length_val': [], 'attr_idx': []}
+                for l in range(self._config['batch_size']):
+                    batch['user_idx'].append([self._user_idx])
+                    batch['sequence_length_val'].append([seq_len])
+                    item_indices, attr_indices = self._generate_user_item_attr(self._user_idx, seq_len)
+                    batch['item_idx'].append(item_indices)
+                    batch['attr_idx'].append(attr_indices)
+                    self._user_idx += 1
+                json.dump(batch, fout)
+                fout.write('\n')
