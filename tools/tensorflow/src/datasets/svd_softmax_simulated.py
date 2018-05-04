f549d352a2b3c6bc12e885c194f1ae74eb6deb2f
qian2015 <zhaox331@umn.edu>
2018-05-04 00:12:24 -0500

    updated simulated

--- tools/tensorflow/src/datasets/svd_softmax_simulated.py
+++ tools/tensorflow/src/datasets/svd_softmax_simulated.py
@@ -12,34 +12,48 @@ logger = logging.getLogger('simulated')
 class SVDSoftmaxSimulatedDataSet(DataSet):
 
     def __init__(self, config=None):
-        if config is None:
-            self._config = {
-                'embedding_dim': 5,
-                'user_vocab': 8,
-                'item_vocab': 100,
-                'attr_vocab': 10,
-                'max_seq_len': 10,
-                'min_seq_len': 2,
-                'batch_size': 4,
-                'page_size': 1,
-                'hierarchical': False,
-                'json_file': 'json_file.json',
-            }
-        else:
-            self._config = config
+        self._config = {
+            'embedding_dim': 5,
+            'user_vocab': 8,
+            'item_vocab': 100,
+            'attr_vocab': 10,
+            'max_seq_len': 10,
+            'min_seq_len': 2,
+            'batch_size': 4,
+            'page_size': 1,
+            'hierarchical': False,
+            'json_file': 'json_file.json',
+            'item_attr_file': 'item_attr.txt',
+            'item_weights_file': 'item_weights.txt',
+            'gamma_shape': 100.0,
+            'cluster_weight': 0.0,
+        }
+        if config is not None:
+            self._config.update(config)
         self._user_idx = 0
         self._item2attr = None
-        self._embeddings = {
+        self._item2attr, self._attr2items = self._generate_item_attr()
+        self._weights = {
             'user': np.random.rand(self._config['user_vocab'], self._config['embedding_dim']),
-            'item': np.random.rand(self._config['item_vocab'], self._config['embedding_dim']),
             'attr': np.random.rand(self._config['attr_vocab'], self._config['embedding_dim']),
         }
-        self._item2attr, self._attr2items = self._generate_item_attr()
+        self._weights['item'] = self._generate_item_weights(self._weights['attr'], self._item2attr)
+
+    def _generate_item_weights(self, attr_weights, item2attr):
+        cluster_weights = []
+        for attr_idx in item2attr:
+            cluster_weights.append(list(np.random.nrand(self._config['embedding_dim']) + attr_weights[attr_idx]))
+        random_weights = np.random.rand(self._config['item_vocab'], self._config['embedding_dim'])
+        return ((1.0 - self._config['cluster_weight']) * random_weights +
+                self._config['cluster_weight'] * cluster_weights)
 
     def _generate_item_attr(self):
-        item2attr = [random.randint(0, self._config['attr_vocab'] - 1) for _ in range(
-            self._config['item_vocab'])]
-        attr2items = []
+        p = np.random.dirichlet(np.random.gamma(self._config['gamma_shape'], 1.0, size=[self._config['attr_vocab']]))
+        item2attr = np.random.choice(range(self._config['attr_vocab']), size=[self._config['item_vocab']], p=p)
+        attr2items = [[] for _ in range(self._config['attr_vocab'])]
+        for item_idx in range(len(item2attr)):
+            attr_idx = item2attr[item_idx]
+            attr2items[attr_idx].append(item_idx)
         return item2attr, attr2items
 
     def _generate_from_full_softmax(self, input, weights, size):
@@ -62,18 +76,18 @@ class SVDSoftmaxSimulatedDataSet(DataSet):
     def _generate_user_item_attr(self, user_idx, size):
         if self._config['hierarchical']:
             attr_indices = self._generate_from_full_softmax(
-                self._embeddings['user'][user_idx],
-                self._embeddings['attr'], size)
+                self._weights['user'][user_idx],
+                self._weights['attr'], size)
             item_indices = []
             for attr_idx in attr_indices:
                 item_idx = self._generate_from_sub_softmax(
-                    self._embeddings['attr'][attr_idx],
-                    self._embeddings['item'], self._attr2items[attr_idx])
+                    self._weights['attr'][attr_idx],
+                    self._weights['item'], self._attr2items[attr_idx])
                 item_indices.append(item_idx)
         else:
             item_indices = self._generate_from_full_softmax(
-                self._embeddings['user'][user_idx],
-                self._embeddings['item'], size)
+                self._weights['user'][user_idx],
+                self._weights['item'], size)
             attr_indices = []
             for item_idx in item_indices:
                 attr_indices.append(self._item2attr[item_idx])
