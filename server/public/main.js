acb80d97cf997a40d490db498bd01316d368f114
qian2015 <zhaox331@umn.edu>
2018-07-15 19:30:24 -0400

    both object and code editor

--- server/public/main.js
+++ server/public/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $.getJSON("/config", {}, function(data) {
-    var container = document.getElementById("json-editor");
+    var jsonContainer = document.getElementById("json-editor");
     var options = {};
-    var jsonEditor = new JSONEditor(container, options);
+    var jsonEditor = new JSONEditor(jsonContainer, options);
     jsonEditor.set(data.config);
 
     var codeEditor = ace.edit("code-editor");
-    codeEditor.setTheme("ace/theme/chrome");
+    codeEditor.setTheme("ace/theme/mono_industrial");
     var JsonMode = ace.require("ace/mode/json").Mode;
     codeEditor.session.setMode(new JsonMode());
+
     var value = JSON.stringify(data.config, null, "  ");
     codeEditor.session.setValue(value);
   });
